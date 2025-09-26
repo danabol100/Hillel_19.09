3b5b754dda265e9ace7f9f1e2cfdc4547332e659
Kyrylo Shkarupylo <danabol100@gmail.com>
2025-09-26 22:30:08 +0300

    Add homework3

---
+++ homeworks_3/script.js
@@ -0,0 +1,19 @@
+// Task 1
+
+let word = prompt("Write a word: ");
+let name = prompt("Write your name: ");
+let age = prompt("Write your age: ");
+
+console.log(
+`word: ${word},
+name: ${name},
+age: ${age}`
+);
+
+
+
+///////////////////////////////////////////////
+
+//Task 2 
+// Закоментил сразу для удобства)
+
