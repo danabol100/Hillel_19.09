1b322220ab4ca01ab7ebf35f426e356980959d3f
Kyrylo Shkarupylo <danabol100@gmail.com>
2025-09-27 11:19:41 +0300

    Add task 2

--- homework_3/script.js
+++ homework_3/script.js
@@ -1,14 +1,13 @@
 // Task 1
 
 let word = prompt("Write a word: ");
-let name = prompt("Write your name: ");
+let userName = prompt("Write your name: ");
 let age = prompt("Write your age: ");
 
-console.log(
-`word: ${word},
-name: ${name},
-age: ${age}`
-);
+console.log("Word: ", word);
+console.log("Name: ", userName);
+console.log("Age: ", age);
+
 
 
 
@@ -16,4 +15,9 @@ age: ${age}`
 
 //Task 2 
 // Закоментил сразу для удобства)
+// мне кажеться мы не учили  еще эти методы 
+
+// const number = 10369;
+// let num = String(number).split('').join(' ');
+// console.log(num);
 
