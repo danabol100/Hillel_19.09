9394a4d20cf9d5ef7dc9f59d2b5ae5e995c67f15
Kyrylo Shkarupylo <danabol100@gmail.com>
2025-09-27 13:48:38 +0300

    fix first task

--- homework_3/script.js
+++ homework_3/script.js
@@ -1,23 +1,43 @@
-// Task 1
+//Task 1 
+console.log(typeof 4);
+console.log(typeof "string");
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof {});
+console.log(typeof BigInt);
+console.log(typeof []);
+console.log(typeof function(){
+    return
+});
 
-let word = prompt("Write a word: ");
-let userName = prompt("Write your name: ");
-let age = prompt("Write your age: ");
 
-console.log("Word: ", word);
-console.log("Name: ", userName);
-console.log("Age: ", age);
+
+
+
+
+// Task 2
+
+// let word = prompt("Write a word: ");
+// let userName = prompt("Write your name: ");
+// let age = prompt("Write your age: ");
+
+// console.log(`Word: ${word},
+//     Name: ${userName},
+//     Age: ${age}`);
 
 
 
 
 ///////////////////////////////////////////////
 
-//Task 2 
+//Task 3 
 // Закоментил сразу для удобства)
 // мне кажеться мы не учили  еще эти методы 
 
 // const number = 10369;
 // let num = String(number).split('').join(' ');
 // console.log(num);
+
+
 
