63964058e0710f45f45af513d086480f76704577
Kyrylo Shkarupylo <danabol100@gmail.com>
2025-10-01 11:53:34 +0300

    fix

--- homework_4_2/script.js
+++ homework_4_2/script.js
@@ -2,9 +2,9 @@
 const numbFromUser = prompt("Напишите трехзначное число:");
 
 if (numbFromUser[0] === numbFromUser[1] && numbFromUser[1] === numbFromUser[2] && numbFromUser[0]=== numbFromUser[2]){
-    console.log("Все числа одинаковые")
+    console.log("Все числа одинаковые");
 }else if(numbFromUser[0] === numbFromUser[1] || numbFromUser[1] === numbFromUser[2] ||numbFromUser[0]=== numbFromUser[2]){
-    console.log("Есть одинаковые числа среди введеных!");;
+    console.log("Есть одинаковые числа среди введеных!");
 }else{
     console.log('Числа разные');
 }
