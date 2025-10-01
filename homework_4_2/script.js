66207fb1c5789f432d8c59dd83fd06b8b23f2757
Kyrylo Shkarupylo <danabol100@gmail.com>
2025-10-01 11:50:11 +0300

    add homework 4 2

---
+++ homework_4_2/script.js
@@ -0,0 +1,13 @@
+
+const numbFromUser = prompt("Напишите трехзначное число:");
+
+if (numbFromUser[0] === numbFromUser[1] && numbFromUser[1] === numbFromUser[2] && numbFromUser[0]=== numbFromUser[2]){
+    console.log("Все числа одинаковые")
+}else if(numbFromUser[0] === numbFromUser[1] || numbFromUser[1] === numbFromUser[2] ||numbFromUser[0]=== numbFromUser[2]){
+    console.log("Есть одинаковые числа среди введеных!");;
+}else{
+    console.log('Числа разные');
+}
+
+
+
