0cbf1a08f58c9347c2bcb466b4b522c4097b0c04
Kyrylo Shkarupylo <danabol100@gmail.com>
2025-10-01 13:01:49 +0300

    Add homework 4 3

---
+++ homework_4_3/script.js
@@ -0,0 +1,49 @@
+
+const userAge = prompt("Напишіть Ваш рік народження!");
+switch (userAge){
+    case null:
+        alert("Шкода, що Ви не захотіли ввести свій рік народження");
+        break;
+    default:
+        alert(`Ви народились  в ${userAge} році!`);
+}
+
+
+const userCity = prompt("Напишіть Вашу місто!");
+switch(userCity){
+    case "Київ":
+    case "Вашингтон":
+    case "Лондон":
+        alert(`Ти живеш у столиці ${userCity}`);
+        break;
+    case null:
+        alert("Шкода, що Ви не захотіли ввести свое місто");
+        break;
+    default:
+        alert(`ти живеш у місті ${userCity}`);
+}
+    
+
+
+
+
+const userHobby = prompt("Напишить Ваш улюблений вид спорту!");
+
+switch(userHobby){
+    case "футбол":
+        alert(`Круто! Хочеш стати Роналдо ?`);
+        break;
+    case "бокс":
+        alert(`Круто! Хочеш стати Олександром Усиком ?`);
+        break;
+    case "плавання":
+        alert(`Круто! Хочеш стати Майклом Фелпсом ?`);
+        break;
+    case null:
+        alert("Шкода, що Ви не захотіли ввести свій спорт!");
+        break;
+    default:
+        alert(`Ваш улюблений вид спорту ${userHobby}`);
+
+}
+
