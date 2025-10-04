ade25e8ec7d140326175a35e0c41efe56c440a10
Kyrylo Shkarupylo <danabol100@gmail.com>
2025-10-04 14:11:05 +0300

    Add homework 5 4

---
+++ homework_5_4/script.js
@@ -0,0 +1,21 @@
+let numbFromUser = +prompt("Введите целое число");
+let flag = false;
+while (numbFromUser <= 0) {
+  numbFromUser = +prompt(" Внимательно! Введите число больше нуля!");
+}
+if (numbFromUser === 1) {
+  console.log("число 1 не простое");
+} else {
+  for (let i = 2; i < numbFromUser; i++) {
+    if (numbFromUser % i === 0) {
+      flag = true;
+      break;
+    }
+  }
+
+  if (flag) {
+    console.log(`число ${numbFromUser} не простое`);
+  } else {
+    console.log(`число ${numbFromUser} простое`);
+  }
+}
