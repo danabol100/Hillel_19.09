8ebe86020ea516059de862cc50fc04c6bab7a7b3
Kyrylo Shkarupylo <danabol100@gmail.com>
2025-10-04 12:15:31 +0300

    Add homework 5 2

---
+++ homework_5_2/script.js
@@ -0,0 +1,26 @@
+const curs = 26;
+
+// ЦЫКЛ While!!!
+
+// let numb = 10;
+// while (numb <= 100) {
+//   let result = curs * numb;
+//   console.log(`${numb}$ это ${result} гривень`);
+//   numb += 10;
+// }
+
+//Цыкл DO WHILE!
+
+// let numb = 10;
+// do {
+//   let result = curs * numb;
+//   console.log(`${numb}$ это ${result} гривень`);
+//   numb += 10;
+// } while (numb <= 100);
+
+//ЦЫКЛ FOR!!
+
+for (let i = 10; i <= 100; i += 10) {
+  let result = curs * i;
+  console.log(`${i}$ это ${result} гривень`);
+}
