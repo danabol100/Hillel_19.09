05d8503b7be6dfaec0ef31885006249bd1827b62
Kyrylo Shkarupylo <danabol100@gmail.com>
2025-11-01 19:09:05 +0200

    Add homework 13

---
+++ homework_13/script.js
@@ -0,0 +1,32 @@
+const form = document.querySelector("form");
+
+const inputs = document.querySelectorAll("input, textarea");
+const errors = document.querySelectorAll("p");
+
+const checkValue = (value, regEx, item) => {
+  if (regEx.test(value)) {
+    item.style.display = "none";
+  } else {
+    item.style.display = "block";
+  }
+};
+const validateValue = (event) => {
+  event.preventDefault();
+
+  inputs.forEach((input, index) => {
+    if (index === 0) {
+      checkValue(input.value, /^.+$/, errors[0]);
+    }
+    if (index === 1) {
+      checkValue(input.value, /.{5,}/, errors[1]);
+    }
+    if (index === 2) {
+      checkValue(input.value, /^\+380\d+$/, errors[2]);
+    }
+    if (index === 3) {
+      checkValue(input.value, /.+\@\w+\./, errors[3]);
+    }
+  });
+};
+
+form.addEventListener("submit", validateValue);
