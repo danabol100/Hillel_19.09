bd9f78078a5361ab863181adc4ce273ca3623b44
Kyrylo Shkarupylo <danabol100@gmail.com>
2025-12-13 18:57:26 +0200

    add homework_23

---
+++ homework_23/src/App.jsx
@@ -0,0 +1,11 @@
+import Form from "./components/Form";
+import Field from "./components/Field";
+
+function App() {
+  return  <main className="container">
+    <Form/>
+    <Field/>
+  </main>;
+   
+}
+export default App;
---
+++ homework_23/src/components/Form.jsx
@@ -0,0 +1,9 @@
+const Form = () => {
+    return <form action="submit" className="d-flex mb-2">
+        <div className="https">https://swapi.dev/api/</div>
+        <input className="form-control w-100" type="text" placeholder="bllabla/" />
+          <button className="btn btn-primary">request</button>
+        </form>
+
+}
+export default Form;
